d1c921e6a87be0f842f9158b790b98b68a6e75ac
table681 <63261999+table681@users.noreply.github.com>
2024-05-08 08:53:40 +0100

    Uploaded files to google drive

--- functions/create-stripe-payment.js
+++ functions/create-stripe-payment.js
@@ -1,22 +1,23 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const serverless = require('serverless-http');
-const express = require('express');
-const cors = require('cors');
-const multer = require('multer');
-const { processFile } = require('./upload'); // Import the processFile function from upload.js
-const { google } = require('googleapis');
-const fs = require('fs').promises; // Add this line to require the 'fs' module
-const path = require('path'); // Add this line to require the 'path' module
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const serverless = require("serverless-http");
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const { processFile } = require("./upload"); // Import the processFile function from upload.js
+const { google } = require("googleapis");
+// const fs = require("fs").promises; // Add this line to require the 'fs' module
+// const path = require("path"); // Add this line to require the 'path' module
+const { Readable } = require("stream"); // Create stream from buffer to upload files to google drive
 
 const app = express();
 app.use(express.json());
 
 const allowedOrigins = [
-  'http://localhost:8888',
-  'https://lucky-liger-cadc9d.netlify.app',
-  'https://eduardos-stupendous-site-4488f5.webflow.io',
-  'https://www.typewriters.ai',
-  'https://jovial-treacle-09f7aa.netlify.app',
+  "http://localhost:8888",
+  "https://lucky-liger-cadc9d.netlify.app",
+  "https://eduardos-stupendous-site-4488f5.webflow.io",
+  "https://www.typewriters.ai",
+  "https://jovial-treacle-09f7aa.netlify.app",
 ];
 
 const corsOptions = {
@@ -24,10 +25,10 @@ const corsOptions = {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
@@ -35,98 +36,211 @@ app.use(cors(corsOptions));
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-app.post('/.netlify/functions/create-stripe-payment', upload.fields([{ name: 'file' }, { name: 'contextFile' }]), async (req, res) => {
+// Function create temporarily folder in google drive
+const driveFolderCreate = async ({
+  drive,
+  parentsFolderId = "1YCcOYDFGrIp2owr0HHBD2QwxL1s27PB4" /* Replace it with google drive folder id containing temporary */,
+}) => {
   try {
-    console.log('Starting create-stripe-payment function');
-
-    const file = req.files['file'][0];
-    const contextFile = req.files['contextFile'] ? req.files['contextFile'][0] : null;
-    const turnaroundTime = req.body.turnaroundTime;
-    const quality = req.body.quality;
-
-    if (!file) {
-      res.status(400).json({ error: 'No file uploaded.' });
-      return;
-    }
-
-    console.log('File received, processing file...');
-
-    const price = await processFile(file, turnaroundTime, quality);
-    const priceInCents = Math.round(price * 100);
-
-    console.log('File processed, creating Stripe price object...');
-
-    // Create the priceObject
-    const priceObject = await stripe.prices.create({
-      unit_amount: priceInCents,
-      currency: 'usd',
-      product_data: {
-        name: 'Fixed Payment',
-      },
+    // Create a folder in Google Drive to store the uploaded files
+    const folderName = `Order_${Date.now()}`; // Use a unique folder name based on the current timestamp
+    const folderMetadata = {
+      name: folderName,
+      mimeType: "application/vnd.google-apps.folder",
+      parents: [parentsFolderId],
+    };
+    const folder = await drive.files.create({
+      resource: folderMetadata,
+      fields: "id, webViewLink",
     });
-
-    console.log('Stripe price object created, generating file ID...');
-
-    // Generate a unique identifier for the files
-    const fileId = Date.now().toString();
-
-    console.log('File ID generated, storing files temporarily...');
-
-    // Store the files temporarily on the server
-    const tempFiles = [file, contextFile].filter(Boolean);
-    await Promise.all(tempFiles.map((tempFile, index) => {
-      const tempFileName = `${fileId}_${index}_${tempFile.originalname}`;
-      const tempFilePath = path.join('/tmp', tempFileName);
-      return fs.writeFile(tempFilePath, tempFile.buffer);
-    }));
-
-    console.log('Files stored temporarily, creating Stripe payment link...');
-
-    const paymentLink = await stripe.paymentLinks.create({
-      line_items: [
-        {
-          price: priceObject.id,
-          quantity: 1,
-        },
-      ],
-      allow_promotion_codes: true,
-      payment_intent_data: {
-        capture_method: 'automatic',
-      },
-      metadata: {
-        fileId: fileId,
-        businessSpecific: quality === 'Business specific',
-        turnaroundTime: turnaroundTime,
-      },
-    });
-
-    console.log('Stripe payment link created:', paymentLink);
-
-    res.json({ paymentLink: paymentLink.url });
+    const folderId = folder.data.id;
+    const folderLink = folder.data.webViewLink;
+    console.log(
+      "Temporarily folder created in Google Drive with ID:",
+      folderId
+    );
+    console.log("Temporarily folder link:", folderLink);
+    return folderId;
   } catch (error) {
-    console.error('Error generating Stripe payment link:', error);
-    res.status(500).json({ error: 'Failed to generate Stripe payment link' });
+    console.log("Unable to create temporary folder in Google drive:", error);
+    return null;
   }
-});
+};
 
-app.post('/.netlify/functions/stripe-webhook', async (req, res) => {
-  const sig = req.headers['stripe-signature'];
+// Function upload file to temporarily folder in google drive
+const driveUpload = async ({
+  drive,
+  driveFolderId,
+  fileName,
+  fileMineType,
+  buffer,
+}) => {
+  try {
+    const fileMetadata = {
+      name: fileName,
+      parents: [driveFolderId], // Use the ID of the created folder
+    };
+
+    // Create a Readable stream from the buffer
+    const readableStream = new Readable();
+    readableStream.push(buffer);
+    readableStream.push(null); // Indicates the end of the stream
+
+    const media = {
+      mimeType: fileMineType,
+      body: readableStream,
+    };
+    await drive.files.create({
+      resource: fileMetadata,
+      media: media,
+      fields: "id",
+    });
+    console.log("Temporarily file uploaded to Google Drive:", fileName);
+  } catch (error) {
+    console.error("Error uploading temporarily files to Google Drive:", error);
+  }
+};
+
+app.post(
+  "/.netlify/functions/create-stripe-payment",
+  upload.fields([{ name: "file" }, { name: "contextFile" }]),
+  async (req, res) => {
+    try {
+      console.log("Starting create-stripe-payment function");
+
+      const file = req.files["file"][0];
+      const contextFile = req.files["contextFile"]
+        ? req.files["contextFile"][0]
+        : null;
+      const turnaroundTime = req.body.turnaroundTime;
+      const quality = req.body.quality;
+
+      if (!file) {
+        res.status(400).json({ error: "No file uploaded." });
+        return;
+      }
+
+      console.log("File received, processing file...");
+
+      const price = await processFile(file, turnaroundTime, quality);
+      const priceInCents = Math.round(price * 100);
+
+      console.log("File processed, creating Stripe price object...");
+
+      // Create the priceObject
+      const priceObject = await stripe.prices.create({
+        unit_amount: priceInCents,
+        currency: "usd",
+        product_data: {
+          name: "Fixed Payment",
+        },
+      });
+
+      console.log("Stripe price object created, generating file ID...");
+
+      // Generate a unique identifier for the files
+      // const fileId = Date.now().toString();
+
+      console.log("File ID generated, storing files temporarily...");
+
+      // Store the files temporarily on the server
+      const tempFiles = [file, contextFile].filter(Boolean);
+
+      // Initialize Google Drive API client
+      const auth = new google.auth.GoogleAuth({
+        credentials: JSON.parse(
+          process.env.GOOGLE_APPLICATION_CREDENTIALS.replace(/'/g, '"')
+        ),
+        scopes: ["https://www.googleapis.com/auth/drive"],
+      });
+      const drive = google.drive({ version: "v3", auth });
+      console.log("Google Drive API client initialized");
+
+      // Create a folder containing new temporary files for ordering in google drive
+      const driveFolderId = await driveFolderCreate({
+        drive,
+      });
+      // If unable to create a folder containing temporary files in Google Drive, return an error
+      if (driveFolderId === null) {
+        throw Error("Unable to create temporary folder in Google drive");
+      }
+
+      await Promise.all(
+        tempFiles.map(async (tempFile, index) => {
+          // Add files uploaded by users to temporary folder in google drive
+          const tempFileName = tempFile.originalname;
+          const tempFileMineType = tempFile.mimetype;
+          // const tempFilePath = path.join("/tmp", tempFileName);
+          // return fs.writeFile(tempFilePath, tempFile.buffer);
+
+          // Now click on the "Pay with Stripe" button on the website,
+          // the code will upload the file to the temporary folder in Google drive
+          // In some cases if the file is large in size, loading can take a long time,
+          // which can mislead the user into thinking that the code is not running.
+          // Uploading files to google's temporary directory can be done asynchronously by changing "await driveUpload" to "driveUpload",
+          // This allows the payment link generation to occur simultaneously while uploading the file.
+          await driveUpload({
+            drive,
+            driveFolderId,
+            fileName: tempFileName,
+            fileMineType: tempFileMineType,
+            buffer: tempFile.buffer,
+          });
+        })
+      );
+
+      console.log("Files stored temporarily, creating Stripe payment link...");
+
+      const paymentLink = await stripe.paymentLinks.create({
+        line_items: [
+          {
+            price: priceObject.id,
+            quantity: 1,
+          },
+        ],
+        allow_promotion_codes: true,
+        payment_intent_data: {
+          capture_method: "automatic",
+        },
+        metadata: {
+          fileId: driveFolderId, // Replace fileId to driveFolderId (google drive folder id) containing temporary files, used for processing when users make payments
+          businessSpecific: quality === "Business specific",
+          turnaroundTime: turnaroundTime,
+        },
+      });
+
+      console.log("Stripe payment link created:", paymentLink);
+
+      res.json({ paymentLink: paymentLink.url });
+    } catch (error) {
+      console.error("Error generating Stripe payment link:", error);
+      res.status(500).json({ error: "Failed to generate Stripe payment link" });
+    }
+  }
+);
+
+app.post("/.netlify/functions/stripe-webhook", async (req, res) => {
+  const sig = req.headers["stripe-signature"];
   let stripeEvent;
 
   try {
-    stripeEvent = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
-    console.log('Stripe event constructed successfully');
+    stripeEvent = stripe.webhooks.constructEvent(
+      req.body,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+    console.log("Stripe event constructed successfully");
   } catch (err) {
-    console.error('Error constructing Stripe event:', err);
+    console.error("Error constructing Stripe event:", err);
     return res.status(400).json({ error: `Webhook Error: ${err.message}` });
   }
 
-  if (stripeEvent.type === 'checkout.session.completed') {
-    console.log('Checkout session completed event received');
+  if (stripeEvent.type === "checkout.session.completed") {
+    console.log("Checkout session completed event received");
     const session = stripeEvent.data.object;
 
-    if (session.payment_status === 'paid') {
-      console.log('Payment status: paid');
+    if (session.payment_status === "paid") {
+      console.log("Payment status: paid");
       // Retrieve the files and details from the session metadata
       const files = JSON.parse(session.metadata.files);
       const businessSpecific = session.metadata.businessSpecific;
@@ -136,25 +250,27 @@ app.post('/.netlify/functions/stripe-webhook', async (req, res) => {
       try {
         // Initialize Google Drive API client
         const auth = new google.auth.GoogleAuth({
-          credentials: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS.replace(/'/g, '"')),
-          scopes: ['https://www.googleapis.com/auth/drive'],
+          credentials: JSON.parse(
+            process.env.GOOGLE_APPLICATION_CREDENTIALS.replace(/'/g, '"')
+          ),
+          scopes: ["https://www.googleapis.com/auth/drive"],
         });
-        const drive = google.drive({ version: 'v3', auth });
-        console.log('Google Drive API client initialized');
+        const drive = google.drive({ version: "v3", auth });
+        console.log("Google Drive API client initialized");
 
         // Create a folder in Google Drive to store the uploaded files
         const folderName = `Order_${Date.now()}`; // Use a unique folder name based on the current timestamp
         const folderMetadata = {
           name: folderName,
-          mimeType: 'application/vnd.google-apps.folder',
-          parents: ['1jOZcw-_tr71AuFq55YckTWeYg5VpjEYy'], // Add the parent folder ID here
+          mimeType: "application/vnd.google-apps.folder",
+          parents: ["1jOZcw-_tr71AuFq55YckTWeYg5VpjEYy"], // Add the parent folder ID here
         };
         const folder = await drive.files.create({
           resource: folderMetadata,
-          fields: 'id',
+          fields: "id",
         });
         const folderId = folder.data.id;
-        console.log('Folder created in Google Drive with ID:', folderId);
+        console.log("Folder created in Google Drive with ID:", folderId);
 
         // Upload each file to the created folder in Google Drive
         for (const file of files) {
@@ -165,14 +281,14 @@ app.post('/.netlify/functions/stripe-webhook', async (req, res) => {
             };
             const media = {
               mimeType: file.mimetype,
-              body: Buffer.from(file.buffer, 'base64'),
+              body: Buffer.from(file.buffer, "base64"),
             };
             await drive.files.create({
               resource: fileMetadata,
               media: media,
-              fields: 'id',
+              fields: "id",
             });
-            console.log('File uploaded to Google Drive:', file.originalname);
+            console.log("File uploaded to Google Drive:", file.originalname);
           }
         }
 
@@ -183,36 +299,44 @@ app.post('/.netlify/functions/stripe-webhook', async (req, res) => {
           turnaroundTime,
           timestamp: new Date().toISOString(),
         };
-        const orderDetailsFileName = 'order_details.json';
+        const orderDetailsFileName = "order_details.json";
         const orderDetailsFileMetadata = {
           name: orderDetailsFileName,
           parents: [folderId],
-          mimeType: 'application/json',
+          mimeType: "application/json",
         };
         const orderDetailsFileMedia = {
-          mimeType: 'application/json',
+          mimeType: "application/json",
           body: JSON.stringify(orderDetails),
         };
         await drive.files.create({
           resource: orderDetailsFileMetadata,
           media: orderDetailsFileMedia,
-          fields: 'id',
+          fields: "id",
         });
-        console.log('Order details stored in Google Drive');
+        console.log("Order details stored in Google Drive");
 
-        return res.status(200).json({ message: 'Payment successfully processed and files uploaded to Google Drive' });
+        return res.status(200).json({
+          message:
+            "Payment successfully processed and files uploaded to Google Drive",
+        });
       } catch (error) {
-        console.error('Error uploading files to Google Drive:', error);
-        return res.status(500).json({ error: 'Payment processed, but an error occurred while uploading files to Google Drive' });
+        console.error("Error uploading files to Google Drive:", error);
+        return res.status(500).json({
+          error:
+            "Payment processed, but an error occurred while uploading files to Google Drive",
+        });
       }
     } else {
-      console.log('Payment not completed');
-      return res.status(200).json({ message: 'Payment not completed' });
+      console.log("Payment not completed");
+      return res.status(200).json({ message: "Payment not completed" });
     }
   }
 
-  console.log('Unhandled event type:', stripeEvent.type);
-  return res.status(400).json({ error: `Unhandled event type: ${stripeEvent.type}` });
+  console.log("Unhandled event type:", stripeEvent.type);
+  return res
+    .status(400)
+    .json({ error: `Unhandled event type: ${stripeEvent.type}` });
 });
 
 module.exports.handler = serverless(app);
--- functions/stripe-webhook.js
+++ functions/stripe-webhook.js
@@ -1,94 +1,95 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const { google } = require('googleapis');
-const fs = require('fs').promises;
-const path = require('path');
-const mime = require('mime-types');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const { google } = require("googleapis");
+// const fs = require("fs").promises;
+// const path = require("path");
+// const mime = require('mime-types');
+
+const moveFolder = async ({ drive, folderId, newParentId }) => {
+  const drive = google.drive({ version: "v3", auth });
+  try {
+    // Get current information of the directory
+    const folderMetadata = await drive.files.get({
+      fileId: folderId,
+      fields: "parents",
+    });
+
+    // Get the list of current parents of the directory
+    const currentParents = folderMetadata.data.parents.join(",");
+
+    // Move folder into new folder by deleting old parent and adding new parent
+    await drive.files.update({
+      fileId: folderId,
+      addParents: newParentId,
+      removeParents: currentParents,
+      fields: "id, parents",
+    });
+
+    console.log(`Folder ${folderId} has been moved to ${newParentId}.`);
+  } catch (error) {
+    throw Error(`Error moving folder: ${error}`);
+  }
+};
+
+const deleteFolder = async ({ drive, folderId }) => {
+  try {
+    await drive.files.delete({
+      fileId: folderId,
+    });
+
+    console.log(`Folder with ID ${folderId} has been deleted.`);
+  } catch (error) {
+    console.error("Error deleting folder:", error);
+  }
+};
 
 exports.handler = async (event, context) => {
-  const sig = event.headers['stripe-signature'];
+  const sig = event.headers["stripe-signature"];
   let stripeEvent;
 
   try {
-    stripeEvent = stripe.webhooks.constructEvent(event.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
-    console.log('Stripe event constructed successfully');
+    stripeEvent = stripe.webhooks.constructEvent(
+      event.body,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+    console.log("Stripe event constructed successfully");
   } catch (err) {
-    console.error('Error constructing Stripe event:', err);
+    console.error("Error constructing Stripe event:", err);
     return {
       statusCode: 400,
       body: `Webhook Error: ${err.message}`,
     };
   }
 
-  if (stripeEvent.type === 'checkout.session.completed') {
-    console.log('Checkout session completed event received');
+  if (stripeEvent.type === "checkout.session.completed") {
+    console.log("Checkout session completed event received");
     const session = stripeEvent.data.object;
 
-    if (session.payment_status === 'paid') {
-      console.log('Payment status: paid');
+    // Initialize Google Drive API client
+    const auth = new google.auth.GoogleAuth({
+      credentials: JSON.parse(
+        process.env.GOOGLE_APPLICATION_CREDENTIALS.replace(/'/g, '"')
+      ),
+      scopes: ["https://www.googleapis.com/auth/drive"],
+    });
+    const drive = google.drive({ version: "v3", auth });
+    console.log("Google Drive API client initialized");
+
+    if (session.payment_status === "paid") {
+      console.log("Payment status: paid");
       const businessSpecific = session.metadata.businessSpecific;
       const cost = session.amount_total / 100; // Convert from cents to dollars
       const turnaroundTime = session.metadata.turnaroundTime;
       const fileId = session.metadata.fileId;
 
       try {
-        // Initialize Google Drive API client
-        const auth = new google.auth.GoogleAuth({
-          credentials: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS.replace(/'/g, '"')),
-          scopes: ['https://www.googleapis.com/auth/drive'],
+        const folderId = "1jOZcw-_tr71AuFq55YckTWeYg5VpjEYy"; // Change to google drive folder id containing official files (After the user has successfully paid)
+
+        await moveFolder({
+          drive,
+          folderId: fileId,
+          newParentId: folderId,
         });
-        const drive = google.drive({ version: 'v3', auth });
-        console.log('Google Drive API client initialized');
-
-        // Create a folder in Google Drive to store the uploaded files
-        const folderName = `Order_${Date.now()}`; // Use a unique folder name based on the current timestamp
-        const folderMetadata = {
-          name: folderName,
-          mimeType: 'application/vnd.google-apps.folder',
-          parents: ['1jOZcw-_tr71AuFq55YckTWeYg5VpjEYy'],
-        };
-        const folder = await drive.files.create({
-          resource: folderMetadata,
-          fields: 'id, webViewLink',
-        });
-        const folderId = folder.data.id;
-        const folderLink = folder.data.webViewLink;
-        console.log('Folder created in Google Drive with ID:', folderId);
-        console.log('Folder link:', folderLink);
-
-        // Retrieve the temporarily stored files
-        const tempFiles = [];
-        for (let i = 0; i < 2; i++) {
-          const tempFileName = `${fileId}_${i}_`;
-          const tempFilePath = path.join('/tmp', tempFileName);
-          const tempFileExists = await fs.access(tempFilePath).then(() => true).catch(() => false);
-          if (tempFileExists) {
-            const tempFileBuffer = await fs.readFile(tempFilePath);
-            const tempFile = {
-              name: tempFileName.replace(/^[^_]+_[^_]+_/, ''),
-              data: tempFileBuffer.toString('base64'),
-              type: mime.lookup(tempFileName) || 'application/octet-stream',
-            };
-            tempFiles.push(tempFile);
-          }
-        }
-
-        // Upload each file to the created folder in Google Drive
-        for (const file of tempFiles) {
-          const fileMetadata = {
-            name: file.name,
-            parents: [folderId], // Use the ID of the created folder
-          };
-          const media = {
-            mimeType: file.type,
-            body: Buffer.from(file.data, 'base64'),
-          };
-          await drive.files.create({
-            resource: fileMetadata,
-            media: media,
-            fields: 'id',
-          });
-          console.log('File uploaded to Google Drive:', file.name);
-        }
 
         // Store the additional details in a file in Google Drive
         const orderDetails = {
@@ -97,58 +98,50 @@ exports.handler = async (event, context) => {
           turnaroundTime,
           timestamp: new Date().toISOString(),
         };
-        const orderDetailsFileName = 'order_details.json';
+        const orderDetailsFileName = "order_details.json";
         const orderDetailsFileMetadata = {
           name: orderDetailsFileName,
           parents: [folderId],
-          mimeType: 'application/json',
+          mimeType: "application/json",
         };
         const orderDetailsFileMedia = {
-          mimeType: 'application/json',
+          mimeType: "application/json",
           body: JSON.stringify(orderDetails),
         };
         await drive.files.create({
           resource: orderDetailsFileMetadata,
           media: orderDetailsFileMedia,
-          fields: 'id',
+          fields: "id",
         });
-        console.log('Order details stored in Google Drive');
-
-        // Delete the temporarily stored files
-        await Promise.all(tempFiles.map((tempFile) => {
-          const tempFileName = `${fileId}_${tempFiles.indexOf(tempFile)}_${tempFile.name}`;
-          const tempFilePath = path.join('/tmp', tempFileName);
-          return fs.unlink(tempFilePath);
-        }));
+        console.log("Order details stored in Google Drive");
 
         return {
           statusCode: 200,
-          body: 'Payment successfully processed and files uploaded to Google Drive',
+          body: "Payment successfully processed and files uploaded to Google Drive",
         };
       } catch (error) {
-        console.error('Error uploading files to Google Drive:', error);
+        console.error("Error uploading files to Google Drive:", error);
         return {
           statusCode: 500,
-          body: 'Payment processed, but an error occurred while uploading files to Google Drive',
+          body: "Payment processed, but an error occurred while uploading files to Google Drive",
         };
       }
     } else {
-      console.log('Payment not completed');
-
+      console.log("Payment not completed");
+      const fileId = session.metadata.fileId;
       // Delete the temporarily stored files
-      for (let i = 0; i < 2; i++) {
-        const tempFileName = `${fileId}_${i}_`;
-        const tempFilePath = path.join('/tmp', tempFileName);
-        await fs.unlink(tempFilePath).catch(() => {});
-      }
+      await deleteFolder({
+        drive,
+        folderId: fileId,
+      });
       return {
         statusCode: 200,
-        body: 'Payment not completed',
+        body: "Payment not completed",
       };
     }
   }
 
-  console.log('Unhandled event type:', stripeEvent.type);
+  console.log("Unhandled event type:", stripeEvent.type);
   return {
     statusCode: 400,
     body: `Unhandled event type: ${stripeEvent.type}`,
